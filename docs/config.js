40c99370067ce6505b4c7a5ed4d2cf69a4358e7e
Generoso Martello <generoso.martello@gmail.com>
2018-07-13 00:53:28 +0200

    updated website

--- docs/config.js
+++ docs/config.js
@@ -2,5 +2,5 @@ zuix.store("config", {
     "title": "zKit, components for modern web.",
     "googleSiteId": "UA-123-456",
     "resourcePath": "./",
-    "libraryPath": "http://192.168.2.104:8081/lib"
+    "libraryPath": "./lib"
 });
