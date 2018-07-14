f807aef615e70c5c92b40dbfdea6f4738e8d215b
Generoso Martello <generoso.martello@gmail.com>
2018-07-14 23:38:58 +0200

    fix config.js

--- docs/config.js
+++ docs/config.js
@@ -5,11 +5,10 @@ zuix.store("config", {
     "libraryPath": "/lib"
 });
 
-    // Check that service workers are registered
-    if ('serviceWorker' in navigator) {
-        // Use the window load event to keep the page load performant
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('./service-worker.js');
-        });
-    }
-    
+// Check that service workers are registered
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/zkit/service-worker.js');
+    });
+}
