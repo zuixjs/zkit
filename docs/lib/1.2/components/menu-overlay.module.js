677a5a8da5d15696e1f98913228ccd76b583bee5
Gene <genemars@users.noreply.github.com>
2022-06-08 22:01:01 +0200

    Updated zuix version.

--- docs/lib/1.2/components/menu-overlay.module.js
+++ docs/lib/1.2/components/menu-overlay.module.js
@@ -1,4 +1,4 @@
-import 'https://cdn.jsdelivr.net/npm/zuix-dist@1.1.7/js/zuix.module.min.js';
+import 'https://cdn.jsdelivr.net/npm/zuix-dist@1.1.8/js/zuix.module.min.js';
 customElements.define('menu-overlay', class extends HTMLElement {
   connectedCallback() {
     zuix.loadComponent(this, 'https://zuixjs.github.io/zkit/lib/1.2/components/menu-overlay');
--- docs/lib/1.2/controllers/shadow-view.module.js
+++ docs/lib/1.2/controllers/shadow-view.module.js
@@ -1,4 +1,4 @@
-import 'https://cdn.jsdelivr.net/npm/zuix-dist@1.1.7/js/zuix.module.min.js';
+import 'https://cdn.jsdelivr.net/npm/zuix-dist@1.1.8/js/zuix.module.min.js';
 customElements.define('shadow-view', class extends HTMLElement {
   connectedCallback() {
     zuix.loadComponent(this, 'default', 'ctrl', {
