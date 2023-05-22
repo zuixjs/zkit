ca8f9599fa670af19dd4848b8a3fbeb869b2f032
Gene <genemars@users.noreply.github.com>
2023-05-22 12:49:09 +0200

    Removed GA

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,6 @@
   zuix.store('config', {
     "title": "<code>zKit</code> components",
     "subtitle": "web enhancing bits",
-    "googleSiteId": "UA-116384214-1",
     "baseUrl": "/zkit/",
     "resourcePath": "/zkit/app/",
     "libraryPath": {
@@ -23,7 +22,8 @@
       "libraryPath": "https://zuixjs.github.io/zkit/lib/1.2/",
       "zuixVersion": "1.1.27"
     },
-    "siteMapUrl": "https://zuixjs.github.io/zkit"
+    "siteMapUrl": "https://zuixjs.github.io/zkit",
+    "googleSiteId": null
   });
   // Check that service workers are registered
   if ('serviceWorker' in navigator) {
